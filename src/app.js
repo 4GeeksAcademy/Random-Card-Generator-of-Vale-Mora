adc80b7d1492d14f4fa50cf6491196d50fa21c6e
valentinamora <148815425+valentinamora@users.noreply.github.com>
2024-05-17 19:17:30 +0000

    mis cambios

--- src/app.js
+++ src/app.js
@@ -2,10 +2,58 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const numbersOfCards = [
+  "A",
+  "J",
+  "Q",
+  "K",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10"
+];
+const cardsSuits = ["♦", "♥", "♠", "♣"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  const randomCards = document.getElementById("Cards");
+
+  let randomNumbersOfCards =
+    numbersOfCards[Math.floor(Math.random() * numbersOfCards.length)];
+  let randomCardsSuits =
+    cardsSuits[Math.floor(Math.random() * cardsSuits.length)];
+
+  const leftSuit = document.createElement("div");
+  randomCards.appendChild(leftSuit);
+  leftSuit.innerHTML = randomCardsSuits;
+  leftSuit.style.fontSize = "90px";
+  leftSuit.style.paddingLeft = "15px";
+
+  const newBodyOfCard = document.createElement("div");
+  randomCards.appendChild(newBodyOfCard);
+  newBodyOfCard.innerHTML = randomNumbersOfCards;
+  newBodyOfCard.style.fontSize = "90px";
+  newBodyOfCard.style.textAlign = "center";
+
+  const rightSuit = document.createElement("div");
+  randomCards.appendChild(rightSuit);
+  rightSuit.innerHTML = randomCardsSuits;
+  rightSuit.style.fontSize = "90px";
+  rightSuit.style.textAlign = "start";
+  rightSuit.style.transform = "rotate(180deg)";
+  rightSuit.style.paddingLeft = "15px";
+
+  if (randomCardsSuits === "♦" || randomCardsSuits === "♥") {
+    newBodyOfCard.style.color = "red";
+    leftSuit.style.color = "red";
+    rightSuit.style.color = "red";
+  } else {
+    newBodyOfCard.style.color = "black";
+    leftSuit.style.color = "black";
+    rightSuit.style.color = "black";
+  }
 };
